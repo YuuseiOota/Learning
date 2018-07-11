15c80061818993a7336e5984c67271d3a99b1367
YuuseiOota <porte0625@gmail.com>
2018-07-11 21:06:47 +0900

    Initial Commit

---
+++ chatdev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chatdev
+//
+//  Created by UxU on 2018/07/11.
+//  Copyright © 2018年 UxU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
